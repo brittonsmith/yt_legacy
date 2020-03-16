2d3faa0eaae54c5f1d0924cc4bc74595b4295127
Britton Smith <brittonsmith@gmail.com>
2020-03-16 13:59:22 +0000

    Rename package to yt_legacy.

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
-setup(name="yt_gadget_fof_plus",
+setup(name="yt_legacy",
       version="1.0",
-      description="An extension to the yt gadget_fof frontend with additional support for halo member particle fields.",
+      description="A yt extension for the Legacy project.",
       author="Britton Smith",
       author_email="brittonsmith@gmail.com",
       license="BSD",
-      url="http://bitbucket.org/brittonsmith/yt_gadget_fof_plus",
-      packages=["yt_gadget_fof_plus"],
+      url="https://github.com/brittonsmith/yt_legacy",
+      packages=["yt_legacy"],
       classifiers=[
           "Development Status :: 2 - Pre-Alpha",
           "Environment :: Console",
