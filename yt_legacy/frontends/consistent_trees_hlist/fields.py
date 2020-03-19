a0bdf31b562bbcd04d6759974d516c375dcdbddd
Britton Smith <brittonsmith@gmail.com>
2020-03-19 15:59:42 +0000

    Add some field aliases.

--- yt_legacy/frontends/consistent_trees_hlist/fields.py
+++ yt_legacy/frontends/consistent_trees_hlist/fields.py
@@ -4,12 +4,20 @@ from yt.fields.field_info_container import \
 m_units = "code_mass"
 p_units = "code_length"
 v_units = "code_velocity"
-e_units = "code_mass * code_velocity**2"
-a_units = "code_velocity / code_time"
+r_units = "kpc/h"
 
 class ConsistentTreesHListFieldInfo(FieldInfoContainer):
     known_other_fields = (
     )
 
     known_particle_fields = (
+        ("id", ("", ["particle_identifier"], None)),
+        ("x", (p_units, ["particle_position_x"], None)),
+        ("y", (p_units, ["particle_position_y"], None)),
+        ("z", (p_units, ["particle_position_z"], None)),
+        ("vx", (v_units, ["particle_velocity_x"], None)),
+        ("vy", (v_units, ["particle_velocity_y"], None)),
+        ("vz", (v_units, ["particle_velocity_z"], None)),
+        ("Mvir", (m_units, ["particle_mass"], None)),
+        ("Rvir", (r_units, ["virial_radius"], None)),
     )
